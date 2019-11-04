7c8c1fb415d53f73b219ec7e5abf54c6366edaec
daoj <daoj@uwindsor.ca>
2019-11-04 02:03:22 +0000

    set up add-user form

--- hotelapp/urls.py
+++ hotelapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('reservation-form', views.registration_form, name='registrationForm'),
+    path('new-customer', views.new_customer_form, name='new-customer')
 ]
--- hotelapp/views.py
+++ hotelapp/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms
+
 
 def index(request):
     return HttpResponse("Welcome to the hotel reservation app!")
 
+
+def registration_form(request):
+    if request.method == 'POST':
+        form = forms.reserveForm(request.POST)
+        if form.is_valid():
+            reservation_instance = form.save(commit=True)
+    else:
+        form = forms.reserveForm()
+    return render(request, 'hotelapp/reservation_form.html', {'form': form})
+
+
+def new_customer_form(request):
+    if request.method == 'POST':
+        form = forms.createUserForm(request.POST)
+        if form.is_valid():
+            # TODO should add confirmation to web page for adding new user
+            reservation_instance = form.save(commit=True)
+    else:
+        form = forms.createUserForm()
+    return render(request, 'hotelapp/new_user.html', {'form': form})
